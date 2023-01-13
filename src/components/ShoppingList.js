ad359f7af4aa7ae296583ddfcba8e4882e54afd7
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-13 18:26:01 +0300

    Completed Assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,35 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [input, setInput] = useState("");
+  const [list, setList] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleInputChange(event) {
+    setInput(event.target.value);
+  }
 
-    return item.category === selectedCategory;
+  function updateList(newInput) {
+    setList([...list, newInput]);
+  }
+
+  const itemsToDisplay = list.filter((item) => {
+    if (selectedCategory === "All" && input === "") return true;
+
+    return item.category === selectedCategory || item.name.indexOf(input) > -1;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={updateList} />
+      <Filter
+        onSearchChange={handleInputChange}
+        search={input}
+        onCategoryChange={handleCategoryChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
